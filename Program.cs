b85dd595e0b89e717078d079148573562dcb556c
Jack <jack.w.mccall@gmail.com>
2020-06-01 11:31:47 +1000

    Initialise repo

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace JamToast
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
